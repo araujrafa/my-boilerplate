3f66ccf9a6d100d93a210493df8197d362664bba
rafael.araujo <araujrafa@gmail.com>
2017-02-10 11:15:18 -0200

    rodar sass e browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,25 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: './',
+      index: 'index.html'
+    }
+  });
+
+  gulp.watch("*.html").on("change", browserSync.reload);
+  gulp.watch("sass/*.scss", ['sass'], browserSync.reload);
+});
 
 gulp.task('default', function () {
 
